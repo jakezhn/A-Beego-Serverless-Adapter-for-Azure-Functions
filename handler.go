abf2cedfa177d006948dd44f6755ba771726d050
jakezhn <64647378+jakezhn@users.noreply.github.com>
2023-12-06 16:50:32 -0500

    mod handler and host for debug

--- handler.go
+++ handler.go
@@ -1,64 +1,60 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
-
-	azfuncAdapter "github.com/jakezhn/Beego-Azure-Functions-Deployment-Adapter/serverlessAdapter"
-
-	"github.com/beego/beego/v2/server/web"
+	"os"
 )
 
-// Controller is controller for BeegoApp
-type MyController1 struct {
-	web.Controller
-	routeInfo map[string]string
-}
+// // Controller is controller for BeegoApp
+// type MyController1 struct {
+// 	web.Controller
+// 	routeInfo map[string]string
+// }
 
-type MyController2 struct {
-	web.Controller
-	routeInfo map[string]string
-}
+// type MyController2 struct {
+// 	web.Controller
+// 	routeInfo map[string]string
+// }
 
-// Define the Get method of Beego controller, response to HTTP GET request
-func (ctrl *MyController1) Get() {
-	// Print "Hello from Beego Controller" as response body
-	ctrl.Ctx.WriteString("Hello from Beego Controller!")
-}
+// // Define the Get method of Beego controller, response to HTTP GET request
+// func (ctrl *MyController1) Get() {
+// 	// Print "Hello from Beego Controller" as response body
+// 	ctrl.Ctx.WriteString("Hello from Beego Controller!")
+// }
 
-func (ctrl *MyController2) Post() {
-	// Beego automatically checks the HTTP method, so no need to check it here
+// func (ctrl *MyController2) Post() {
+// 	// Beego automatically checks the HTTP method, so no need to check it here
 
-	if ctrl.Ctx.Request.Method != "POST" {
-		ctrl.Ctx.Output.SetStatus(405) // HTTP 405 Method Not Allowed
-		ctrl.Ctx.Output.Body([]byte("Only POST method is accepted"))
-		return
-	}
-	// Parse JSON body into a map
-	var data map[string]interface{}
-	body, err := io.ReadAll(ctrl.Ctx.Request.Body)
-	if err != nil {
-		ctrl.Ctx.Output.SetStatus(500)
-		ctrl.Ctx.Output.Body([]byte("Error reading request body"))
-		return
-	}
+// 	if ctrl.Ctx.Request.Method != "POST" {
+// 		ctrl.Ctx.Output.SetStatus(405) // HTTP 405 Method Not Allowed
+// 		ctrl.Ctx.Output.Body([]byte("Only POST method is accepted"))
+// 		return
+// 	}
+// 	// Parse JSON body into a map
+// 	var data map[string]interface{}
+// 	body, err := io.ReadAll(ctrl.Ctx.Request.Body)
+// 	if err != nil {
+// 		ctrl.Ctx.Output.SetStatus(500)
+// 		ctrl.Ctx.Output.Body([]byte("Error reading request body"))
+// 		return
+// 	}
 
-	defer ctrl.Ctx.Request.Body.Close()
-	if err := json.Unmarshal(body, &data); err != nil {
-		ctrl.Ctx.Output.SetStatus(400)
-		ctrl.Ctx.Output.Body([]byte("Error parsing JSON body"))
-		return
-	}
+// 	defer ctrl.Ctx.Request.Body.Close()
+// 	if err := json.Unmarshal(body, &data); err != nil {
+// 		ctrl.Ctx.Output.SetStatus(400)
+// 		ctrl.Ctx.Output.Body([]byte("Error parsing JSON body"))
+// 		return
+// 	}
 
-	// Iterate over the map and output the values
-	for _, value := range data {
-		if value == "How about you?" {
-			ctrl.Ctx.WriteString(fmt.Sprintf("%v", "Whatsup from Azure Functions!"))
-		}
-	}
-}
+// 	// Iterate over the map and output the values
+// 	for _, value := range data {
+// 		if value == "How about you?" {
+// 			ctrl.Ctx.WriteString(fmt.Sprintf("%v", "Whatsup from Azure Functions!"))
+// 		}
+// 	}
+// }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello from azfunction!")
@@ -76,8 +72,19 @@ func main() {
 	// azfuncAdapter.HttpFuncsHandler(Ctrl1.routeInfo, Ctrl1, "MyController1")
 	// azfuncAdapter.HttpFuncsHandler(Ctrl2.routeInfo, Ctrl2, "MyController2")
 
+	// Setting up default port listening
+	//azfuncAdapter.PortHandler(":8080")
+
 	http.HandleFunc("/api/hello", hello)
 
-	// Setting up default port listening
-	azfuncAdapter.PortHandler(":8080")
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+		log.Printf("Custom handler port detected, using port: %s", val)
+	}
+
+	// Start the HTTP server and listen on the specified address
+	if err := http.ListenAndServe(listenAddr, nil); err != nil {
+		log.Fatalf("Failed to start HTTP server on %s: %v", listenAddr, err)
+	}
 }
