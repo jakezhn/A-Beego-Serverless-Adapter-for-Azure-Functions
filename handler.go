66f0b0c29c27ea9a02c338c3fd2611e8e2c89710
jakezhn <64647378+jakezhn@users.noreply.github.com>
2023-12-06 17:44:55 -0500

    prep handler

--- handler.go
+++ handler.go
@@ -1,90 +1,80 @@
 package main
 
 import (
+	azfuncAdapter "beegoserverless/serverlessAdapter"
+	"encoding/json"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
-	"os"
+
+	"github.com/beego/beego/v2/server/web"
 )
 
-// // Controller is controller for BeegoApp
-// type MyController1 struct {
-// 	web.Controller
-// 	routeInfo map[string]string
-// }
+// Controller is controller for BeegoApp
+type MyController1 struct {
+	web.Controller
+	routeInfo map[string]string
+}
 
-// type MyController2 struct {
-// 	web.Controller
-// 	routeInfo map[string]string
-// }
+type MyController2 struct {
+	web.Controller
+	routeInfo map[string]string
+}
 
-// // Define the Get method of Beego controller, response to HTTP GET request
-// func (ctrl *MyController1) Get() {
-// 	// Print "Hello from Beego Controller" as response body
-// 	ctrl.Ctx.WriteString("Hello from Beego Controller!")
-// }
+// Define the Get method of Beego controller, response to HTTP GET request
+func (ctrl *MyController1) Get() {
+	// Print "Hello from Beego Controller" as response body
+	ctrl.Ctx.WriteString("Hello from Beego Controller!")
+}
 
-// func (ctrl *MyController2) Post() {
-// 	// Beego automatically checks the HTTP method, so no need to check it here
+func (ctrl *MyController2) Post() {
+	// Beego automatically checks the HTTP method, so no need to check it here
 
-// 	if ctrl.Ctx.Request.Method != "POST" {
-// 		ctrl.Ctx.Output.SetStatus(405) // HTTP 405 Method Not Allowed
-// 		ctrl.Ctx.Output.Body([]byte("Only POST method is accepted"))
-// 		return
-// 	}
-// 	// Parse JSON body into a map
-// 	var data map[string]interface{}
-// 	body, err := io.ReadAll(ctrl.Ctx.Request.Body)
-// 	if err != nil {
-// 		ctrl.Ctx.Output.SetStatus(500)
-// 		ctrl.Ctx.Output.Body([]byte("Error reading request body"))
-// 		return
-// 	}
+	if ctrl.Ctx.Request.Method != "POST" {
+		ctrl.Ctx.Output.SetStatus(405) // HTTP 405 Method Not Allowed
+		ctrl.Ctx.Output.Body([]byte("Only POST method is accepted"))
+		return
+	}
+	// Parse JSON body into a map
+	var data map[string]interface{}
+	body, err := io.ReadAll(ctrl.Ctx.Request.Body)
+	if err != nil {
+		ctrl.Ctx.Output.SetStatus(500)
+		ctrl.Ctx.Output.Body([]byte("Error reading request body"))
+		return
+	}
 
-// 	defer ctrl.Ctx.Request.Body.Close()
-// 	if err := json.Unmarshal(body, &data); err != nil {
-// 		ctrl.Ctx.Output.SetStatus(400)
-// 		ctrl.Ctx.Output.Body([]byte("Error parsing JSON body"))
-// 		return
-// 	}
+	defer ctrl.Ctx.Request.Body.Close()
+	if err := json.Unmarshal(body, &data); err != nil {
+		ctrl.Ctx.Output.SetStatus(400)
+		ctrl.Ctx.Output.Body([]byte("Error parsing JSON body"))
+		return
+	}
 
-// 	// Iterate over the map and output the values
-// 	for _, value := range data {
-// 		if value == "How about you?" {
-// 			ctrl.Ctx.WriteString(fmt.Sprintf("%v", "Whatsup from Azure Functions!"))
-// 		}
-// 	}
-// }
+	// Iterate over the map and output the values
+	for _, value := range data {
+		if value == "How about you?" {
+			ctrl.Ctx.WriteString(fmt.Sprintf("%v", "Whatsup from Azure Functions!"))
+		}
+	}
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello from azfunction!")
 }
 
 func main() {
-	// // Create controllers and set up routers' indo (path and action)
-	// Ctrl1 := &MyController1{routeInfo: make(map[string]string)}
-	// Ctrl1.routeInfo["/api/hello"] = "Get"
+	// Create controllers and set up routers' indo (path and action)
+	Ctrl1 := &MyController1{routeInfo: make(map[string]string)}
+	Ctrl1.routeInfo["/api/hello"] = "Get"
 
-	// Ctrl2 := &MyController2{routeInfo: make(map[string]string)}
-	// Ctrl2.routeInfo["/api/whatsup"] = "Post"
+	Ctrl2 := &MyController2{routeInfo: make(map[string]string)}
+	Ctrl2.routeInfo["/api/whatsup"] = "Post"
 
-	// // Register routers and http functions
-	// azfuncAdapter.HttpFuncsHandler(Ctrl1.routeInfo, Ctrl1, "MyController1")
-	// azfuncAdapter.HttpFuncsHandler(Ctrl2.routeInfo, Ctrl2, "MyController2")
+	// Register routers and http functions
+	azfuncAdapter.HttpFuncsHandler(Ctrl1.routeInfo, Ctrl1, "MyController1")
+	azfuncAdapter.HttpFuncsHandler(Ctrl2.routeInfo, Ctrl2, "MyController2")
 
-	// Setting up default port listening
-	//azfuncAdapter.PortHandler(":8080")
-
-	http.HandleFunc("/api/hello", hello)
-
-	listenAddr := ":8080"
-	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
-		listenAddr = ":" + val
-		log.Printf("Custom handler port detected, using port: %s", val)
-	}
-
-	// Start the HTTP server and listen on the specified address
-	if err := http.ListenAndServe(listenAddr, nil); err != nil {
-		log.Fatalf("Failed to start HTTP server on %s: %v", listenAddr, err)
-	}
+	//Setting up default port listening
+	azfuncAdapter.PortHandler(":8080")
 }
