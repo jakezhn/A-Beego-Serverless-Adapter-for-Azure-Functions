0ef282da0add7ea7cf58fa341afd917b9cdb890f
jakezhn <64647378+jakezhn@users.noreply.github.com>
2023-12-07 11:02:53 -0500

    mod demo

--- demo/BeegoServerlessExamples/shorturl/handler.go
+++ demo/BeegoServerlessExamples/shorturl/handler.go
@@ -3,18 +3,15 @@ package main
 import (
 	azfuncAdapter "beegoserverless/serverlessAdapter"
 
-	"github.com/beego/samples/shorturl/controllers"
+	"test/controllers"
 )
 
 func main() {
-	mainCtrl := &controllers.MainController{routeInfo: make(map[string]string)}
-	mainCtrl.routeInfo["/"] = "Get"
 	shortCtrl := &controllers.ShortController{routeInfo: make(map[string]string)}
-	shortCtrl.routeInfo["/v1/shorten"] = "Get"
+	shortCtrl.routeInfo["/api/shorten"] = "Get"
 	expandtrl := &controllers.ExpandController{routeInfo: make(map[string]string)}
-	expandtrl.routeInfo["/v1/expand"] = "Get"
+	expandtrl.routeInfo["/api/expand"] = "Get"
 
-	azfuncAdapter.HttpFuncsHandler(mainCtrl.routeInfo, mainCtrl, "main")
 	azfuncAdapter.HttpFuncsHandler(shortCtrl.routeInfo, shortCtrl, "shorturl")
 	azfuncAdapter.HttpFuncsHandler(shortCtrl.routeInfo, shortCtrl, "expandurl")
 	azfuncAdapter.PortHandler(":8080")
